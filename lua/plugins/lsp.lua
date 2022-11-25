6234f2b8981595a892222b48cdcd997921e623ed
RamPanic <67488206+RamPanic@users.noreply.github.com>
2022-11-25 12:42:02 -0300

    Perl and Python LS added

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,13 +1,5 @@
-local status_ok, lsp_installer = pcall(require, 'nvim-lsp-installer')
-if not status_ok then
-    return
-end
-
-local status_ok, lsp_config = pcall(require, 'lspconfig')
-if not status_ok then
-    return
-end
-
+local lsp_installer = require('nvim-lsp-installer')
+local lsp_config = require('lspconfig')
 
 -- Configurar instalador LSP
 
@@ -26,3 +18,27 @@ lsp_config["sumneko_lua"].setup({
         }
     }
 })
+
+
+-- Configurar Python LSP
+
+lsp_config["pyright"].setup({})
+
+
+-- Configurar Perl LSP
+
+local perltidy_profile = os.getenv("HOME") .. '/.config/nvim/others/.perltidyrc'
+local perlcritic_profile = os.getenv("HOME") .. '/.config/nvim/others/.perlcriticrc'
+
+lsp_config["perlnavigator"].setup{
+    cmd = {'node', '/opt/nvim/repos/PerlNavigator/server/out/server.js', '--stdio'},
+    settings = {
+      perlnavigator = {
+          perlPath = 'perl',
+          enableWarnings = true,
+          perlcriticEnabled = true,
+          perltidyProfile = perltidy_profile,
+          perlcriticProfile = perlcritic_profile
+      }
+    }
+}
